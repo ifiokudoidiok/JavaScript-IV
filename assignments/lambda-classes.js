72f4d42fce0acb4d35e23c7ae17542c909350f29
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-08-22 14:14:47 +0100

    Instructor class Attr assigned

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,20 +1,48 @@
 // CODE here for your Lambda Classes
+
 class Person {
+    
     constructor(data){
         this.name= data.name;
         this.age= data.age;
         this.location= data.location;
     }
+    
     speak(){
-        return `Hello my name is ${this.name} I am from ${this.location}`;
+        return `Hello my name is ${this.name} \n I am from ${this.location}`;
     }
 };
+
+let sam = new Person ({name:'Sam', age:45, location:'italy'},'Redux');
+
+
 class Instructors extends Person{
     constructor(data){
         super(data);
+        this.speciality= [];
+        this.favLanguage= [];
+        this.catchPhrase=  "";
+    }
+    speciality(talent){
+        return this.stomach.push(talent)
+      }
+      favLanguage(fav){
+        return this.stomach.push(fav)
+      }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+    grade(student,subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`)
     }
 
 };
+let jules = new Instructors ({name:'Jules', age:25, location:'London'},this.speciality="Don't forget the homies");
+jules.speciality = ['redux'];
+jules.favLanguage = ['JS', 'Pyhton', 'Elm'];
+jules.catchPhrase = "Don't forget the homies";
+
+
 class Students extends Person{
     constructor(data){
         super(data);
