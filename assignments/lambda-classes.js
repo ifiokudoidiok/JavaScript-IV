545ea4c11ada73aae6072439f745260d623261ed
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-08-22 12:55:46 +0100

    Lambda Classes structure set up

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,25 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(data){
+
+    }
+};
+class Instructors extends Person{
+    constructor(data){
+        super(data);
+    }
+
+};
+class Students extends Person{
+    constructor(data){
+        super(data);
+    }
+
+};
+class ProjectManagers extends Instructors{
+    constructor(data){
+        super(data);
+
+    }
+
+};
