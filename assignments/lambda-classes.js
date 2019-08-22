d752f37ff902d6d680600275e7158d7d603ad0b1
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-08-22 12:59:07 +0100

    Person class Attr assigned

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,7 +1,12 @@
 // CODE here for your Lambda Classes
 class Person {
     constructor(data){
-
+        this.name= data.name;
+        this.age= data.age;
+        this.location= data.location;
+    }
+    speak(){
+        return `Hello my name is ${this.name} I am from ${this.location}`;
     }
 };
 class Instructors extends Person{
